cc08a2ae85b9133d7250e3bec874009f033e7223
kittiphop sompuech <admin@kittiphops-MacBook-Air.local>
2026-02-14 00:56:08 +0700

    update docker

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://our-love-story-backend.up.railway.app/api/v1';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
